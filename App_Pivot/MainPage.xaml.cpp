8623ed5cb0961e1f3c61783529827ccc1c47b61d
Kazu-SK <sonodans109@gmail.com>
2025-04-04 23:20:25 +0900

    プロジェクト ファイルを追加します。

---
+++ App_Pivot/MainPage.xaml.cpp
@@ -0,0 +1,36 @@
+﻿//
+// MainPage.xaml.cpp
+// MainPage クラスの実装。
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace App_Pivot;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+void MainPage::OnSettingButtonClick(Object^ sender, RoutedEventArgs^ e)
+{
+    auto dialog = ref new ContentDialog();
+    dialog->Title = "設定";
+    dialog->Content = "設定ボタンが押されました。";
+    dialog->CloseButtonText = "OK";
+    dialog->ShowAsync();
+}
